fb62b5a4a18042117b3a9c3a7b817ddb5304f583
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-07-27 20:33:52 -0400

    moved context menu creation

--- src/drawer/context/PathTitledTab.java
+++ src/drawer/context/PathTitledTab.java
@@ -1,8 +1,5 @@
 package drawer.context;
 
-import java.io.IOException;
-import javafx.scene.control.ContextMenu;
-import javafx.scene.control.MenuItem;
 import javafx.scene.control.TitledPane;
 
 public class PathTitledTab extends TitledPane {
@@ -13,22 +10,14 @@ public class PathTitledTab extends TitledPane {
 	public PathTitledTab() {
 		setText(String.format("Path %d", index++));
 
-		MenuItem rename = new MenuItem("Rename");
-		rename.setOnAction(event -> {
-			try {
-				this.setText(RenameDialog.display(this.getText()));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
 
-		ContextMenu contextMenu = new ContextMenu(rename);
-		setContextMenu(contextMenu);
-
-		setContent(new PathTable(keyPoints));
 	}
 
 	public PathGroup getKeyPoints() {
 		return keyPoints;
+	}
+
+	public void clear() {
+		keyPoints.clear();
 	}
 }
