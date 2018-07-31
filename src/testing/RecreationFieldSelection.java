d62ac7bf3f8ab9a2502690bf341e46cdebe24257
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-07-31 14:28:39 -0400

    updated jar

--- src/testing/RecreationFieldSelection.java
+++ src/testing/RecreationFieldSelection.java
@@ -9,9 +9,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class RecreationFieldSelection implements Initializable {
 
@@ -34,7 +36,9 @@ public class RecreationFieldSelection implements Initializable {
 
 		pointPlacement.getChildren().addAll(new NotificationArrow(150, 150, "Hello is Marius, What is your? I love you is it mutual?"));
 		pointPlacement.getChildren().addAll(new NotificationArrow(150, 350, "Hello"));
-//		pointPlacement.getChildren().add(new PositionPoint(100, 100));
+
+
+		//		pointPlacement.getChildren().add(new PositionPoint(100, 100));
 
 //		pointPlacement.getChildren().add(polygon);
 	}
