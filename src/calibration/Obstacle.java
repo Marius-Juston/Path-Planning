db669fd9c4879ec88cbdd129a0a8c41975dab800
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-07-31 14:27:24 -0400

    obstacle has to recreate the shape
    issue due to padding between nodes. This is an iissue must fix this

--- src/calibration/Obstacle.java
+++ src/calibration/Obstacle.java
@@ -1,6 +1,8 @@
 package calibration;
 
 import javafx.scene.Group;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
 import javafx.scene.shape.Shape;
 
 public class Obstacle extends Group {
@@ -8,9 +10,17 @@ public class Obstacle extends Group {
 	private final ThreatLevel threatLevel;
 	private final Shape definingShape;
 
-	public Obstacle(ThreatLevel threatLevel, Shape definingShape) {
+	//	TODO fix the problems hat the padding and all cause the shapes to be translated wrongly
+	public Obstacle(ThreatLevel threatLevel, Polygon definingShape) {
 		this.threatLevel = threatLevel;
-		this.definingShape = definingShape;
+		this.definingShape = new Polygon(definingShape.getPoints().stream().mapToDouble(value -> value).toArray());
+		getChildren().add(definingShape);
+	}
+
+	public Obstacle(ThreatLevel threatLevel, Path definingShape) {
+		this.threatLevel = threatLevel;
+
+		this.definingShape = new Path(definingShape.getElements());
 		getChildren().add(definingShape);
 	}
 
