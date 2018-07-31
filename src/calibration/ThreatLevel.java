07ea8da4408acd4de2c46469f86cd1627d18e16b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-07-31 14:26:29 -0400

    threat level has message

--- src/calibration/ThreatLevel.java
+++ src/calibration/ThreatLevel.java
@@ -3,15 +3,17 @@ package calibration;
 import javafx.scene.paint.Color;
 
 public enum ThreatLevel {
-	WARNING(Color.rgb(0, 0, 255, .5), Color.ORANGE), ERROR(Color.gray(.5, .6), Color.RED);
+	WARNING(Color.rgb(0, 0, 255, .5), Color.ORANGE, "Warning the robot is passing on a dangerous place"), ERROR(Color.gray(.5, .6), Color.RED, "Error given the field constrains the robot would not be able to go here");
 
 	private final Color displayColor;
 	private final Color overlayColor;
+	private final String message;
 
-	ThreatLevel(Color displayColor, Color overlayColor) {
+	ThreatLevel(Color displayColor, Color overlayColor, String message) {
 		this.displayColor = displayColor;
 
 		this.overlayColor = overlayColor;
+		this.message = message;
 	}
 
 	public Color getOverlayColor() {
@@ -20,5 +22,9 @@ public enum ThreatLevel {
 
 	public Color getDisplayColor() {
 		return displayColor;
+	}
+
+	public String getMessage() {
+		return message;
 	}
 }
