9e3a8f20ec5cf3f441fdedd809a5dab85af5ccf1
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-08-12 20:17:56 -0400

    in server mode sends values to localhost

--- src/drawer/SplineSender.java
+++ src/drawer/SplineSender.java
@@ -4,10 +4,13 @@ import edu.wpi.first.wpilibj.networktables.NetworkTable;
 
 public class SplineSender {
 
-	public static final String IP_ADDRESS = "10.0.0.24"; //IP address to connect to when in server mode
-	public static final String NETWORK_TABLE_TABLE_KEY = "SmartDashboard"; //network table to send the data to
+	//	TODO make it so that you can change these and it will
+//	public static final String IP_ADDRESS = "10.0.0.24"; //IP address to connect to when in server mode
+	//	public static final String NETWORK_TABLE_TABLE_KEY = "SmartDashboard"; //network table to send the data to
+	public static final String NETWORK_TABLE_TABLE_KEY = String
+		.format("Path Planner - %s", System.getProperty("user.name")); //network table to send the data to
 	private static final int TEAM_NUMBER = 2974; // team number
-	private static final boolean IS_CLIENT = true; // if the program will send to robotRIO or not
+	private static final boolean IS_CLIENT = false; // if the program will send to robotRIO or not
 	public static NetworkTable networkTable;
 
 	private static boolean hasBeenStarted = false;
@@ -16,17 +19,8 @@ public class SplineSender {
 	 * Initializes the network table with ip address settings etc...
 	 */
 	public static void initNetworkTable() {
-//        SettingController.addNumber("TEAM_NUMBER", 2974, NumberType.INTEGER);
-//        SettingController.<Integer>getValue("TEAM_NUMBER").addListener((observable, oldValue, newValue) -> {
-//            if (!newValue.equals(oldValue))
-//            {
-//                NetworkTable.setTeam(newValue);
-//            }
-//        });
 
 		if (!hasBeenStarted) {
-			System.out.println("Hello");
-
 			hasBeenStarted = true;
 
 			if (IS_CLIENT) {
@@ -34,17 +28,17 @@ public class SplineSender {
 				NetworkTable.setTeam(TEAM_NUMBER);
 			} else {
 				NetworkTable.setServerMode();
-				NetworkTable.setIPAddress(IP_ADDRESS);
+//				NetworkTable.setIPAddress(IP_ADDRESS);
+				NetworkTable.setIPAddress("localhost");
 			}
 
 			networkTable = NetworkTable.getTable(NETWORK_TABLE_TABLE_KEY);
+//			networkTable.putString("Hello", "Hello");
 		}
 	}
-
 
 	public static void initNetworkTableParallel() {
 		Thread thread = new Thread(SplineSender::initNetworkTable);
 		thread.start();
-
 	}
 }
