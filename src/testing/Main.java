bf2e66ad64a49dec88308b0a4910fb1f7344a87c
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-08-19 15:15:23 -0400

    udpated example

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -1,10 +1,18 @@
 package testing;
 
+import calibration.Field;
+import calibration.Helper;
 import calibration.obstacle.FieldBorder;
+import calibration.obstacle.Obstacle;
+import calibration.obstacle.ThreatLevel;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.scene.shape.LineTo;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -20,8 +28,27 @@ public class Main extends Application {
   @Override
   public final void start(Stage primaryStage) {
 
-    FieldBorder fieldBorder = new FieldBorder(new Path(new MoveTo(0, 0), new LineTo(3, 3)));
+    try {
+      File file = new File("./src/FRC 2018 Field Drawings.png");
+      Field.getInstance().imageFile = file;
+      Field.getInstance().image = Helper.getImage(file);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
 
+    try {
+
+      Field.getInstance().addObstacle(new Obstacle(ThreatLevel.ERROR, new Polygon(0, 0, 0, 12, 10, 0, 1201, 0)));
+      Field.getInstance().addObstacle(new FieldBorder(new Path(new MoveTo(0, 0), new LineTo(3, 3))));
+      System.out.println(Field.getInstance().getFieldBorder().getDefiningShape());
+
+      Field.getInstance().saveData(new File("save.dat"));
+      Field.getInstance().loadData(new File("save.dat"));
+
+      primaryStage.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
 //    Parent root = FXMLLoader
 //        .load(getClass().getResource("../testing/recreationFieldSelection.fxml"));
 //
--- src/testing/Test.java
+++ src/testing/Test.java
@@ -1,51 +1,63 @@
 package testing;
 
+import calibration.Helper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-import javafx.scene.shape.LineTo;
-import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 class Test {
 
   public static void main(String[] args) {
 
     GsonBuilder builder = new GsonBuilder();
+    Path path;
     Gson gson = builder.create();
 
-    Path path = new Path(new MoveTo(0.0f, 50.0f), new LineTo(100.0f, 100.0f));
+    {
+      Rectangle rectangle = new Rectangle(100, 100);
+      Polygon polygon = new Polygon(0, 0, 50, 50, 0, 50);
+      path = (Path) Shape.subtract(rectangle, polygon);
 
-    try {
-      String completePathObject = gson.toJson(path);
-      System.out.println(completePathObject);
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
-      // java.lang.IllegalArgumentException: class com.sun.javafx.util.WeakReferenceQueue$ListEntry declares multiple JSON fields named next
+      System.out.println(path.getFillRule());
+      System.out.println(path);
+      String s = Helper.convertPathToString(path);
+      System.out.println(s);
+      System.out.println(Helper.convertStringToPath(s));
+
+
     }
 
-    try {
-      String pathObjectElements = gson.toJson(path.getElements());
-      System.out.println(pathObjectElements);
-    } catch (IllegalArgumentException e) {
-      e.printStackTrace();
-      // java.lang.IllegalArgumentException: class com.sun.javafx.util.WeakReferenceQueue$ListEntry declares multiple JSON fields named next
-    }
-
-    try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("test.set"))) {
-      objectOutputStream.writeObject(path);
-    } catch (IOException e) {
-      e.printStackTrace();
-//      java.io.NotSerializableException: javafx.scene.shape.Path
-    }
-
-    try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("test.set"))) {
-      objectOutputStream.writeObject(path.getElements());
-    } catch (IOException e) {
-      e.printStackTrace();
-//      java.io.NotSerializableException: javafx.scene.shape.Path$2
-    }
+//    try {
+//      String completePathObject = gson.toJson(path);
+//      System.out.println(completePathObject);
+//    } catch (IllegalArgumentException e) {
+//      e.printStackTrace();
+//      // java.lang.IllegalArgumentException: class com.sun.javafx.util.WeakReferenceQueue$ListEntry declares multiple JSON fields named next
+//    }
+//
+//    try {
+//      String pathObjectElements = gson.toJson(path.getElements());
+//      System.out.println(pathObjectElements);
+//    } catch (IllegalArgumentException e) {
+//      e.printStackTrace();
+//      // java.lang.IllegalArgumentException: class com.sun.javafx.util.WeakReferenceQueue$ListEntry declares multiple JSON fields named next
+//    }
+//
+//    try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("test.set"))) {
+//      objectOutputStream.writeObject(path);
+//    } catch (IOException e) {
+//      e.printStackTrace();
+////      java.io.NotSerializableException: javafx.scene.shape.Path
+//    }
+//
+//    try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("test.set"))) {
+//      objectOutputStream.writeObject(path.getElements());
+//    } catch (IOException e) {
+//      e.printStackTrace();
+////      java.io.NotSerializableException: javafx.scene.shape.Path$2
+//    }
   }
 }
