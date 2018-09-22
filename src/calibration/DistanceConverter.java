a926a6d0369f55e86ef10a708d4d2b486f6c8804
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-09-22 10:50:20 -0400

    improved distance converter
    now uses the text filter in the input text field to manage user input
    uses regex to retrieve the infromation from the textfield input
    Commented code

--- src/calibration/DistanceConverter.java
+++ src/calibration/DistanceConverter.java
@@ -5,6 +5,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.function.UnaryOperator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,9 +18,14 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.control.TextFormatter.Change;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+/**
+ * Dialog to get the conversion between pixel distance and real length
+ */
 public class DistanceConverter implements Initializable {
 
   private static final Alert confirmation = new Alert(AlertType.CONFIRMATION,
@@ -27,20 +35,32 @@ public class DistanceConverter implements Initializable {
   private static double pixelUnit;
 
   static {
+//    Sets the modality of the window i.e you have to close this program in order to continue
     primaryStage.initModality(Modality.APPLICATION_MODAL);
-//		primaryStage.initStyle(StageStyle.UTILITY);
     primaryStage.setTitle("Unit converter");
   }
 
+  private Pattern unitCapture = Pattern.compile("(\\d+(?:[.\\d]+)?)\\s?([a-zA-Z]+)?");
+  @FXML
+  private TextField dataInput;
   @FXML
   private TextField pixelDistance;
 
+  /**
+   * Displays the {@link DistanceConverter} to make the user define what the scaling is. From pixels to actual units
+   *
+   * @param pixelUnit the pixel distance you want to find the scale of
+   */
   static Unit display(double pixelUnit) throws IOException {
+//    Resets the info
     info = null;
+//    the pixel distance to display
     DistanceConverter.pixelUnit = pixelUnit;
 
+//    Creates the dialog
     Parent root = FXMLLoader.load(DistanceConverter.class.getResource("conversion.fxml"));
 
+//    Displays the dialog
     primaryStage.setScene(new Scene(root));
     primaryStage.showAndWait();
 
@@ -49,26 +69,49 @@ public class DistanceConverter implements Initializable {
 
   @Override
   public final void initialize(URL location, ResourceBundle resources) {
+//    Set the text to the pixel unit
     pixelDistance.setText(String.valueOf(pixelUnit));
 
+//    Added a unit filter so that the text can only be in a certain format otherwise you cannot type it
+    UnaryOperator<Change> objectUnaryOperator = change -> {
+      String newText = change.getControlNewText();
+      if (newText.matches("\\d+(\\.\\d*)?\\s?([A-Za-z]+)?")) {
+        return change;
+      }
+      return null;
+    };
+
+    dataInput.setTextFormatter(new TextFormatter<>(objectUnaryOperator));
   }
 
+  /**
+   * Retrieve the information such as the actual distance and the unit from the textfield and
+   */
   @FXML
   public void retrieveInfo(ActionEvent actionEvent) {
+//    Gets the text from the text field
     TextField text = ((TextField) actionEvent.getSource());
 
+//    If there is text
     if (!text.getText().isEmpty()) {
-      String[] data = text.getText().split(" ");
+//      Match text to pattern
+      Matcher matcher = unitCapture.matcher(text.getText());
 
-      if (Helper.isDouble(data[0])) {
-        info = new Unit(Double.parseDouble(data[0]), Helper.PIXELS);
+//      If there is at least a number
+      if (matcher.find() && matcher.groupCount() >= 1) {
+//        Get the number and convert it to a double
+        info = new Unit(Double.parseDouble(matcher.group(1)), Helper.PIXELS);
 
-        if (data.length >= 2) {
-          info.setUnit(data[1]);
+//        If the user placed units
+        if (matcher.groupCount() >= 2) {
+//          Get the units
+          info.setUnit(matcher.group(2));
         }
 
+//        Show confirmation dialog
         Optional<ButtonType> buttonTypeOptional = confirmation.showAndWait();
         if (buttonTypeOptional.isPresent()) {
+//        If the user says yes then the DistanceConverter dialog will disappear
           if (buttonTypeOptional.get() == ButtonType.YES) {
             Helper.getWindow(actionEvent).hide();
           }
